6715f1e5c861c3ca6d27605f34db64c0eb88f921
rosafreire <46506845+rosafreire@users.noreply.github.com>
2020-01-12 01:21:38 -0300

    Add files via upload

---
+++ Calculadora de Impostos/Calculadora de Impostos/Bonificacoes.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Impostos
+{
+    class Bonificacoes
+    {
+        //classe desenvolvida para cálculo de possiveis descontos
+        public void Descontos(double resultado)
+        {
+            if (resultado < 3.05 * 500)
+            {
+                Console.WriteLine("Não há descontos possíveis nessa compra. Para desconto de 10% adquira mais: " + ( 1525 - resultado) + " em produtos");
+            }
+            if(resultado >= 3.05* 500)
+            {
+                double desconto = resultado * 0.10;
+                Console.WriteLine("Descontos possíveis: " + desconto);
+                Console.WriteLine("Valor Final: " + (resultado - desconto));
+
+            }
+            if (resultado >= 4.5 * 1000)
+            {
+                double desconto = resultado * 0.20;
+                Console.WriteLine("Descontos possíveis: " + desconto);
+                Console.WriteLine("Valor Final: " + (resultado - desconto));
+
+            }
+        }
+    }
+}
---
+++ Calculadora de Impostos/Calculadora de Impostos/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculadora_de_Impostos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc;
+            calc = new Calculadora();
+            string cliente;
+            double quantidade;
+            
+
+            Bonificacoes boni;
+            boni = new Bonificacoes();
+            double resultado;
+
+
+            Interface();
+
+
+            void Interface()
+            {
+                Console.WriteLine("Empresa Compradora: ");
+                cliente = Console.ReadLine();
+                Console.WriteLine("Quantidade Adquirida: ");
+                string qnt = Console.ReadLine();
+                quantidade = Double.Parse(qnt);
+
+                Console.WriteLine("Aqui está o seu resultado... ");
+                calc.TotalDeMercadorias(cliente, quantidade);
+
+                resultado = calc.resultado;
+                Menu(); 
+            }
+
+            void Menu()
+            {
+                Console.WriteLine();
+                Console.WriteLine("Para descontos tecle: D");
+                Console.WriteLine("Para sair tecle: Enter");
+                Console.WriteLine("Para novo cálculo digite: N");
+
+                string Op = Console.ReadLine();
+
+                if (Op == "D" || Op == "d")
+                {
+                    boni.Descontos(resultado);
+                    Menu();
+                    Console.Read();
+                }
+                if (Op == "N" || Op == "n")
+                {
+                    Console.Clear();
+                    Interface();
+                    Menu();
+                }
+                else
+                {
+                    return;
+                }
+            }
+        }
+    }
+}
